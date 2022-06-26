f88eb9d9ef8d745f652c26b6ba8dfba88002757d
Radhika Kedia <radhikakediaa@gmail.com>
2022-06-26 11:10:15 +0530

    26/06/22

---
+++ Recursion/StringtoInteger.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class StringtoInteger {
+    public static int toInteger(String s) {
+
+        
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String s=sc.next();
+        System.out.println(toInteger(s));
+    }
+}
