ba96aad9c0731718178509c778e98ccbb7115a14
Radhika Kedia <radhikakediaa@gmail.com>
2022-06-26 11:09:52 +0530

    26/06/22

--- 2D-Array/PrintSpiral.java
+++ 2D-Array/PrintSpiral.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PrintSpiral {
     public static int[][] input() {
         Scanner sc = new Scanner(System.in);
@@ -12,8 +13,51 @@ public class PrintSpiral {
         }
         return c;
     }
-    public static void printSpiral(int a[][] ) {
-        
+
+    public static void printSpiral1(int a[][] )
+    {
+        int top=0, down=a.length-1, left=0, right=a[0].length-1, dir=0;
+        while(top<=down && left <= right)
+        {
+            if(dir==0)
+            {
+                for(int i=left; i<=right; i++)
+                {
+                    System.out.print(a[top][i]+" ");
+                    
+                }
+                top++;
+            }
+            else if(dir==1)
+            {
+                for(int i=top; i<=down; i++)
+                {
+                System.out.print(a[i][right]+" ");
+                  
+                } 
+                right--;
+            }
+            else if(dir==2)
+            {
+                for(int i=right; i>=left ; i--)
+                {
+                    System.out.print(a[down][i]+" ");
+                   
+                }
+                down--;
+            }
+            else if(dir==3)
+            {
+                for(int i=down; i>=top ; i--)
+                {
+                    System.out.print(a[i][left]+" ");
+                    
+                }
+                left++;
+            }
+            dir =(dir+1)%4;
+        }
+
 
     }
     public static void main(String[] args) {
@@ -21,7 +65,7 @@ public class PrintSpiral {
         int t = sc.nextInt();
         for (int x = 1; x <= t; x++) {
             int a[][] = input();
-            printSpiral(a);
+            printSpiral1(a);
         }
     }
 }
