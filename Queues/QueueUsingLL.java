1e596ae8b8a34fd7a32b1ca8534b0c32cb0e8e5f
Radhika Kedia <radhikakediaa@gmail.com>
2022-07-10 10:01:43 +0530

    10/07/22

--- Queues/QueueUsingLL.java
+++ Queues/QueueUsingLL.java
@@ -1,32 +1,59 @@
 public class QueueUsingLL {
-    
-    private front;
-    private rear;
+
+    private front;private rear;
     private int size;
 
     public QueueUsingLL(){
         private front=null;
         private rear=null;
-        private int size=0;
+
+    private int size = 0;
     }
-    int size(){
+
+    int size() {
         return size;
     }
-    boolean isEmpty(){
-        return size==0;
+
+    boolean isEmpty() {
+        return size == 0;
     }
-    T front(){
-        if(front==null)
-        throw new QueueEmptyException();
+
+    T front() throws QueueEmptyException
+     {
+        if (front == null) //or size==0
+            throw new QueueEmptyException();
 
         return front.data;
     }
-    
+
     void enqueue(T element){
-        
+
         Node<T> newNode = new Node<element>;
 
+        if (size == 0)
+        {
+            front=newNode;
+            rear= newNode;
+            size++;
+        }
+        else{
         rear.next = newNode;
         rear=rear.next;
+        size++;
+        }
+    }
+    T dequeue() throws QueueEmptyException
+    {
+        if(size == 0){
+            throw new QueueEmptyException();
+        }
+        T temp=front.data;
+        front=front.next;
+        if(size==1)
+        {
+            rear=null;
+        }
+        size--;
+        return temp;
     }
 }
